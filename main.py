acd93b5bff4c9fcfb61c69ed3ac104cc487d3408
ZayniddinB <161677226+ZayniddinB@users.noreply.github.com>
2025-02-15 22:44:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from task3 import func, funx
 
 if __name__ == "__main__":
     #task 1
-    kwargsAcceptFun(name="Elyor", car = "BWM", phone = "IPhone")
+    kwargsAcceptFun(name="Zayniddin", car = "Ford", phone = "Samsung")
 
     #task 2
     output = typeBasedTransformer(int_type=3, text_type="Data", boolen_type=True, list_type=[1, 2, 3])
