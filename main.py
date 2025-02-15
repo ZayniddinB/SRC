f78676482fe6d424f2921c22023f9b42d3dd8202
ZayniddinB <161677226+ZayniddinB@users.noreply.github.com>
2025-02-15 22:41:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from task1 import kwargsAcceptFun
+from task2 import typeBasedTransformer
+from task3 import func, funx
+
+
+if __name__ == "__main__":
+    #task 1
+    kwargsAcceptFun(name="Elyor", car = "BWM", phone = "IPhone")
+
+    #task 2
+    output = typeBasedTransformer(int_type=3, text_type="Data", boolen_type=True, list_type=[1, 2, 3])
+    print(output)
+
+    #task 3
+    func()
+    funx()
+    func()
+    funx()
+    func()
